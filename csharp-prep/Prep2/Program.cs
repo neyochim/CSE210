5467b37c35b83a342972f4e5a12e37502501e22a
neyochim <144850597+neyochim@users.noreply.github.com>
2026-01-14 22:56:26 -0700

    Implement grade percentage input and letter grade evaluation

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        int grade = Convert.ToInt32(Console.ReadLine());
+        
+        char letter = ' ';
+        if (grade >= 90)
+        {
+            letter = 'A';
+        }
+        else if (grade >= 80)
+        {
+            letter = 'B';
+        }
+        else if (grade >= 70)
+        {
+            letter = 'C';
+        }
+        else if (grade >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+        Console.WriteLine($"Your letter grade is {letter}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You Passed your class!");
+        }
+        else
+        {
+            Console.WriteLine("Unforunately, you did not pass your class. Better luck next time!");
+        }
     }
 }
