0dabc934c759a86a8a9b8866c794389b8b96cd5e
neyochim <144850597+neyochim@users.noreply.github.com>
2026-02-25 22:36:24 -0700

    Add Assignment, MathAssignment, and WritingAssignment classes with functionality for managing assignments

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,23 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Test base class
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        Console.WriteLine();
+
+        // Test MathAssignment
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        // Test WritingAssignment
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
